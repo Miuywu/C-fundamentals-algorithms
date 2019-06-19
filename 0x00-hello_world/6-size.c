738f626e8b89beb24dba4b57fb1f995b281575a9
Miuywu <859@holbertonschool.com>
2019-06-19 22:55:15 +0000

    use sizeof with placeholder

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -6,13 +6,15 @@
 */
 int main(void)
 {
-int integ, intLong, intLL;
+int integ; 
+long intLong;
+double intLL;
 char chara;
 float flt;
-printf("Size of a char\: sizeof(chara) bytes\(s\)\n");
-printf("Size of int: sizeof(integ) byte\(s\)\n");
-printf("Size of a long int\: sizeof(intLong) byte\(s\)\n");
-printf("Size of a long long int\: sizeof(intLL) byte\(s\)\n");
-printf("Size of a float\: sizeof(flt) byte\(s\)");
+printf("Size of a char: %d byte(s)\n", sizeof(chara));
+printf("Size of an int: %d byte(s)\n", sizeof(integ));
+printf("Size of a long int: %d byte(s)\n", sizeof(intLong));
+printf("Size of a long long int: %d byte(s)\n", sizeof(intLL));
+printf("Size of a float: %d byte(s)\n",  sizeof(flt));
 return (0);
 }
