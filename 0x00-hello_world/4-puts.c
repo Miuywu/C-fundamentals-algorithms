86be6201c2e1ba0915b359f9768b66947a2df6b0
Miuywu <859@holbertonschool.com>
2019-06-19 21:36:41 +0000

    removed comment spaces

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,9 +1,9 @@
 #include <stdio.h>
 /**
- * main - prints required text
- *
- * Return: Always 0 (Success)
- */
+* main - prints required text
+*
+* Return: Always 0 (Success)
+*/
 int main(void)
 {
 puts("\"Programming is like a multilingual puzzle");
