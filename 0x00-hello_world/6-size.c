3ee1938f28e593685995722a7d30ce0183a32005
Miuywu <859@holbertonschool.com>
2019-06-19 22:13:09 +0000

    added sizeof

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,11 +1,18 @@
 #include <stdio.h>
 /**
- * main - prints required text
- *
- * Return: Always 0 (Success)
- */
+* main - prints required text
+*
+* Return: Always 0 (Success)
+*/
 int main(void)
 {
-	printf("Size of a char\: sizeof(char) bytes\(s\)\nSize of int: sizeof(int) byte\(s\)\nSize of a long int\: sizeof(long int) byte\(s\)\nSize of a long long int\: sizeof(long long int) byte\(s\)\nSize of a float\: sizeof(float) byte\(s\)");
-	return (0);
+int integ, intLong, intLL;
+char chara;
+float flt;
+printf("Size of a char\: sizeof(chara) bytes\(s\)\n");
+printf("Size of int: sizeof(integ) byte\(s\)\n");
+printf("Size of a long int\: sizeof(intLong) byte\(s\)\n");
+printf("Size of a long long int\: sizeof(intLL) byte\(s\)\n");
+printf("Size of a float\: sizeof(flt) byte\(s\)");
+return (0);
 }
