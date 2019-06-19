7024ce98a8c7863bd96cfb08955a342ea5d784b8
Miuywu <859@holbertonschool.com>
2019-06-19 19:11:22 +0000

    create 0 1 2 3 4 5 6

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ * main - prints required text
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	puts("\"Programming is like a multilingual puzzel");
+	return (0);
+}
---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ * main - prints required text
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	printf("Size of a char\: 1 bytes\(s\)\nSize of int: 1 byte\(s\)\nSize of a long int\: 8 byte\(s\)\nSize of a long long int\: 8 byte\(s\)\nSize of a float\: 4 byte\(s\)");
+	return (0);
+}
