4af38b6f1fe593e013bf3f9086ebb6bc290f659d
Miuywu <859@holbertonschool.com>
2019-06-20 23:52:36 +0000

    use ascii to print 0-9

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,21 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+
+/**
+* main - prints 0-9
+*
+* Return: Always 0 (Success)
+*/
+int main(void)
+{
+	int i;
+
+	for (i = 48; i <= 57; i++)
+	{
+		putchar (i);
+	}
+	putchar ('\n');
+
+	return (0);
+}
