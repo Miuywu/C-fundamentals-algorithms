1851d8419adddd19e9806c5f3ab55e3445ccd672
Minh-Huy Vu <49765719+Miuywu@users.noreply.github.com>
2019-06-21 20:23:12 -0700

    prints 00-89 with exceptions

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,30 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+
+/**
+* main - prints 00-89 with some exceptions 
+*
+* Return: Always 0 (Success)
+*/
+int main(void)
+{
+	int i;
+	int j;
+
+	for (i = '0'; i <= '9'; i++)
+	{
+		for (j = i + 1; j <= '9'; j++)
+		{
+			putchar (i);
+			putchar (j);
+			if (i != '8' || j != '9')
+			{
+				putchar (44);
+				putchar (32);
+			}
+		}
+	}
+	putchar ('\n');
+	return (0);
+}
