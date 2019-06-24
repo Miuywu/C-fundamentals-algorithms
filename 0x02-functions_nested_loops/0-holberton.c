144a4746b2d9b049b6a6ad0bfe463c0fa4bef3d9
Minh-Huy Vu <49765719+Miuywu@users.noreply.github.com>
2019-06-24 10:50:00 -0700

    create array containing chars in Holberton, for loops thru array then putchar

---
+++ 0x02-functions_nested_loops/0-holberton.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main(void)
+{
+  int str1[] = {'H', 'o', 'l', 'b', 'e', 'r', 't', 'o', 'n'};
+  int n;
+
+  for (n = 0; n < 9; n++)
+    putchar (str1[n]);
+  putchar ('\n');
+	return (0);
+}
