a56bcd2bb60eebf2d840473f3e33f0e9e256fb3f
Minh-Huy Vu <49765719+Miuywu@users.noreply.github.com>
2019-06-24 11:03:34 -0700

    main calls print_alphabet: prints a-z lowercase

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+void print_alphabet(void);
+
+int main(void)
+{
+    print_alphabet();
+    return (0);
+}
+
+void print_alphabet(void)
+{
+  int i;
+  for (i = 'a'; i <= 'z'; i++)
+    putchar (i);
+}
