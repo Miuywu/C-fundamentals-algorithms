652aca291d791f7387b3b656e4c8d397cfcd5bf5
Miuywu <859@holbertonschool.com>
2019-06-25 22:37:56 +0000

    filename fixes

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,12 +1,12 @@
 #include "holberton.h"
-#include "_putchar.c"
+
 /**
- * print_alphabet - prints 'a'-'z'
+ * print_alphabet_x10 - prints 'a'-'z'
  *
  * Return: Always 0 (Success)
  */
 
-void print_alphabet(void)
+void print_alphabet_x10(void)
 {
 	int i;
 	int m;
