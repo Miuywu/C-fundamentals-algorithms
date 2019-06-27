f042a08f413c0647f9563d6458a4f3390ab87c67
Miuywu <859@holbertonschool.com>
2019-06-27 22:13:09 +0000

    fix indents

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -1,4 +1,4 @@
-#include "holberton.h"
+B#include "holberton.h"
 #include <stdio.h>
 
 /**
@@ -12,16 +12,16 @@ int main(void)
 
 	for (n = 1; n <= 100; n++)
 	{
-			if (n % 3 == 0 && n % 5 == 0)
-				printf("FizzBuzz");
-			else if (n % 3 == 0)
-				printf("Fizz");
-			else if (n % 5 == 0)
-				printf("Buzz");
-			else
-				printf("%d", n);		
-			if (n != 100)
-				printf(" ");
+		if (n % 3 == 0 && n % 5 == 0)
+			printf("FizzBuzz");
+		else if (n % 3 == 0)
+			printf("Fizz");
+		else if (n % 5 == 0)
+			printf("Buzz");
+		else
+			printf("%d", n);
+		if (n != 100)
+			printf(" ");
 	}
 	printf("\n");
 	return (0);
