8e8a27d4dc12fb2dc25f7162f4ee044c9b9b043a
Miuywu <859@holbertonschool.com>
2019-07-01 23:15:36 +0000

    returns length of string

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,19 @@
+#include "holberton.h"
+
+/**
+ * _strlen - returns length of str
+ *
+ * Return: length of str
+ * @s: input
+ */
+int _strlen(char *s)
+{
+	int size = 0;
+
+	for(; *s != '\0'; s++)
+	{
+		size++;
+	}
+
+	return size;
+}
