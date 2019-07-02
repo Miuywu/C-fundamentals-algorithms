58726ebdac8e55c6eeeca7229858606230e4d725
Miuywu <859@holbertonschool.com>
2019-07-02 17:09:57 +0000

    fix style

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -10,13 +10,14 @@ void print_rev(char *s)
 {
 	int size = 0;
 	int a;
+
 	if (*s != '\0')
 	{
-		for(; *s != '\0'; s++)
+		for (; *s != '\0'; s++)
 			size++;
 		s = s - size;
-		for(a = size-1; a >= 0; a--)
+		for (a = size - 1; a >= 0; a--)
 			_putchar(s[a]);
 	}
 	_putchar('\n');
-} 
+}
