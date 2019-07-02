adff0e6a227b3fab38edef8333b92d7791a4a746
Miuywu <859@holbertonschool.com>
2019-07-02 17:05:47 +0000

    reverses string then prints

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,22 @@
+#include "holberton.h"
+
+/**
+ * print_rev - prints string reversed
+ *
+ * Return: null
+ * @s: input
+ */
+void print_rev(char *s)
+{
+	int size = 0;
+	int a;
+	if (*s != '\0')
+	{
+		for(; *s != '\0'; s++)
+			size++;
+		s = s - size;
+		for(a = size-1; a >= 0; a--)
+			_putchar(s[a]);
+	}
+	_putchar('\n');
+} 
