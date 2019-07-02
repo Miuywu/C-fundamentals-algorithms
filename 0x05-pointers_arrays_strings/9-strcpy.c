5ab46f25c99a7c61e1bb67337552ff7282a0025d
Miuywu <859@holbertonschool.com>
2019-07-02 22:27:49 +0000

    copies one array into another

---
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,28 @@
+#include "holberton.h"
+#include <stdio.h>
+
+/**
+ * _strcpy - prints n elements of an array
+ *
+ * Return: null
+ * @src: array
+ * @dst: array size
+ */
+char *_strcpy(char *dest, char *src)
+{
+	int c = 0;
+	int d = 0;
+
+	while (src[c] != '\0')
+	{
+		c++;
+	}
+	c++;
+
+	while (d <= c)
+	{
+		dest[d] = src[d];
+		d++;
+	}
+	return (dest);
+}
