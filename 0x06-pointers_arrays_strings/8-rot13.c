554373cabfd674b04f5a2f68daa2ebbae819702d
Miuywu <859@holbertonschool.com>
2019-07-09 06:57:35 +0000

    notime

--- 0x06-pointers_arrays_strings/8-rot13.c
+++ 0x06-pointers_arrays_strings/8-rot13.c
@@ -11,16 +11,11 @@ char *rot13(char *c)
 {
 	char *a = c;
 	int b;
-	int b2;
-	char v[] = "abcdefghijklm";
-	char l[] = "nopqrstuvwxyz";
-	char v2[] = "ABCDEFGHIJKLM";
-	char l2[] = "NOPQRSTUVWXYZ";
+	char v[] = "aAbBcCdDeEfFgGhHiIjJkKlLmM";
+	char l[] = "nNoOpPqQrRsStTuUvVwWxXyYzZ";
 
 	while (*a != '\0')
 	{
-		if (*a >= 'a' && *a <= 'z')
-		{
 		for (b = 0; v[b] != '\0'; b++)
 		{
 			if (v[b] == *a)
@@ -29,18 +24,6 @@ char *rot13(char *c)
 				*a = v[b];
 		}
 		a++;
-		}
-		else if (*a >= 'A' && *a <= 'Z')
-		{
-		for (b2 = 0; v2[b2] != '\0'; b2++)
-		{
-			if (v2[b2] == *a)
-				*a = l2[b2];
-			else if (l2[b2] == *a)
-				*a = v2[b2];
-		}
-		a++;
-		}
 	}
 	return (c);
 }
