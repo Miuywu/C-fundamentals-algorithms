5872eda054958df12c9ebcdf9295b8f119173659
Miuywu <859@holbertonschool.com>
2019-07-09 06:46:30 +0000

    rotate alphas by 13

---
+++ 0x06-pointers_arrays_strings/8-rot13.c
@@ -0,0 +1,29 @@
+#include "holberton.h"
+
+/**
+ * *rot13 - rots all chars by 13
+ *
+ * Return: str rotted
+ * @c: str 1
+ *
+ */
+char *rot13(char *c)
+{
+	char *a = c;
+	int b;
+	char v[] = "abcdefghijklm";
+	char l[] = "nopqrstuvwxyz";
+
+	while (*a != '\0')
+	{
+		for (b = 0; v[b] != '\0'; b++)
+		{
+			if (v[b] == *a)
+				*a = l[b];
+			else if (l[b] == *a)
+				*a = v[b];
+		}
+		a++;
+	}
+	return (c);
+}
