43a631fe2a581154e59f2b7ad785371a3df5f96c
Minh-Huy Vu <49765719+Miuywu@users.noreply.github.com>
2019-07-09 00:01:31 -0700

    added uppercase case

--- 0x06-pointers_arrays_strings/8-rot13.c
+++ 0x06-pointers_arrays_strings/8-rot13.c
@@ -11,11 +11,16 @@ char *rot13(char *c)
 {
 	char *a = c;
 	int b;
+	int b2;
 	char v[] = "abcdefghijklm";
 	char l[] = "nopqrstuvwxyz";
+	char v2[] = "ABCDEFGHIJKLM";
+	char l2[] = "NOPQRSTUVWXYZ";
 
 	while (*a != '\0')
 	{
+		if (*a >= 'a' && *a <= 'z')
+		{
 		for (b = 0; v[b] != '\0'; b++)
 		{
 			if (v[b] == *a)
@@ -24,6 +29,18 @@ char *rot13(char *c)
 				*a = v[b];
 		}
 		a++;
+		}
+		else if (*a >= 'A' && *a <= 'Z')
+		{
+		for (b2 = 0; v2[b2] != '\0'; b2++)
+		{
+			if (v2[b2] == *a)
+				*a = l2[b2];
+			else if (l2[b2] == *a)
+				*a = v2[b2];
+		}
+		a++;
+		}
 	}
 	return (c);
 }
