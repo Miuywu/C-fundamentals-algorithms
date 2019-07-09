9b7de4e96eaf1008851ac1d7843925a0e8b684e2
Miuywu <859@holbertonschool.com>
2019-07-09 06:16:39 +0000

    converts input str to 1337 format

--- 0x06-pointers_arrays_strings/7-leet.c
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -3,31 +3,22 @@
 /**
  * *leet - translates input msg to l33t code
  *
- * Return: null
+ * Return: pointer to str c
  * @c: input
  */
 char *leet(char *c)
 {
-    int a = 0;
-    int b = 0;
-    int d = 0;
+	char *a = c;
+	int b;
+	char v[] = "aAeEoOtTlL";
+	char l[] = "4433007711";
 
-    for (;*(c + a) != '\0'; a++)
-    {
-        char v[20] = "aAeEoOtTlL";
-        char l[20] = "4433007711"
-        b
-        for (; v[b] != '\0'; b++)
-        {
-            if (c[a] == v[b])
-            {
-                for (; d <= 9; d++)
-                {
-                v[d] = l[d];
-                 }
-                *(c + a) = v[b];
-            }
-        }
-    }
-    return (c);
+	while (*a != '\0')
+	{
+		for (b = 0; v[b] != '\0'; b++)
+			if (v[b] == *a)
+				*a = l[b];
+		a++;
+	}
+	return (c);
 }
