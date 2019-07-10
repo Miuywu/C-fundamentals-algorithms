85613cda9fe297cfd559c2ad439b972a4bd845f9
Miuywu <859@holbertonschool.com>
2019-07-10 21:28:56 +0000

    calculates power of x by y

---
+++ 0x08-recursion/4-pow_recursion.c
@@ -0,0 +1,19 @@
+#include "holberton.h"
+#include <stdio.h>
+/**
+ * _pow_recursion - raises x to power of y
+ * Return: int factorial of a given number
+ * @x: intput
+ * @y: power
+ */
+
+int _pow_recursion(int x, int y)
+{
+	if (y < 0)
+		return (-1);
+	if (y == 1)
+	{
+		return (1);
+	}
+	return (x * _pow_recursion(x, y - 1));
+}
