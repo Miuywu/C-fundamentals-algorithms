40189baa58a4b6a9b089efee68a901e40de0e713
Miuywu <859@holbertonschool.com>
2019-07-10 19:04:32 +0000

    prints string in rev

---
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -0,0 +1,19 @@
+#include "holberton.h"
+#include <stdio.h>
+/**
+ * _print_rev_recursion - prints a string in reverse
+ * lenOfS - finds length of array
+ * Return: NULL
+ * 
+ * @s: pointer to array of chars
+ */
+
+void _print_rev_recursion(char *s)
+{
+	if (*s == '\0')
+	{
+		return;
+	}	
+	_print_rev_recursion(s + 1);
+	_putchar(*s);
+}
