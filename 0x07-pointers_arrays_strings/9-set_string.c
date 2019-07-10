4d1bc73874fc7cf9eaab615806af3e57fc0789b4
Miuywu <859@holbertonschool.com>
2019-07-10 00:02:56 +0000

    sets s1 to &s0

---
+++ 0x07-pointers_arrays_strings/9-set_string.c
@@ -0,0 +1,13 @@
+#include "holberton.h"
+
+/**
+ * set_string - sets the value of pointer to char
+ *
+ * Return: pointer to char
+ * @to: input
+ * @s: array
+ */
+void set_string(char **s, char *to)
+{
+	*s = to;
+}
