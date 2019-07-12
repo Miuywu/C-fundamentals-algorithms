f5fa160ec4d16229e33682f9b2dce10c5add4ff9
Minh-Huy Vu <49765719+Miuywu@users.noreply.github.com>
2019-07-12 12:16:43 -0700

    copies on array into another

---
+++ 0x09-static_libraries/1-memcpy.c
@@ -0,0 +1,18 @@
+#include "holberton.h"
+
+/**
+ * *_memcpy - copies mem area
+ *
+ * Return: pointer to char
+ * @dest: input
+ * @src: out
+ * @n: array
+ */
+char *_memcpy(char *dest, char *src, unsigned int n)
+{
+	unsigned int a;
+
+	for (a = 0; a < n; a++)
+		dest[a] = src[a];
+	return (dest);
+}
