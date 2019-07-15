93e1b655b3c35b42f390514661a2544e1e160f84
Miuywu <859@holbertonschool.com>
2019-07-15 06:04:41 +0000

    prints first arguement

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ * main - entry point
+ *
+ * Return: 1 if c is upper, 0 otherwise
+ * @argc: input
+ * @argv: inout
+ */
+int main(int argc, char *argv[])
+{
+	(void) argc;
+
+	printf("%s\n", *argv);
+	return (0);
+}
