70a067244b95101b8e773ec5e8031ff8c2e544a4
Miuywu <859@holbertonschool.com>
2019-07-15 06:07:09 +0000

    multiplies 2 given arguements

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - entry point
+ *
+ * Return: 1 if c is upper, 0 otherwise
+ * @argc: input
+ * @argv: inout
+ */
+int main(int argc, char *argv[])
+{
+	int p;
+
+	if (argc == 3) 
+	{
+		p = atoi(argv[1]) * atoi(argv[2]);
+		printf("%d\n", p);
+	}
+	else
+	{
+		printf("Error\n");
+		return (1);
+	}
+	return (0);
+}
