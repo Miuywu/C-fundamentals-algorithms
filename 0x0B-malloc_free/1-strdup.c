fd3896a02c280a78c4fa5315ee6e6438582d8bd8
Miuywu <859@holbertonschool.com>
2019-07-16 19:43:04 +0000

    fixed mismatched type comparison

--- 0x0B-malloc_free/1-strdup.c
+++ 0x0B-malloc_free/1-strdup.c
@@ -15,14 +15,14 @@ char *_strdup(char *str)
 	int strSize; /*holds len of str*/
 	int a;
 
-	if (*str == NULL)
+	if (str == NULL)
 		return (NULL);
 	strSize = 0;
 	while (str[strSize] != '\0')/* finds len of str */
 		strSize++;
 	array = malloc(strSize * sizeof(char));
 	/* checks if malloc was successful*/
-	if (*array != NULL)
+	if (array != NULL)
 	{
 		for (a = 0; a < strSize; a++)
 			array[a] = str[a];
