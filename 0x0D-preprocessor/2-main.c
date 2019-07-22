0f98bbe6d1d1c2c0da51881c539e8866415fbbdc
Miuywu <859@holbertonschool.com>
2019-07-22 20:41:44 +0000

    prints the name of the file it was compiled from

---
+++ 0x0D-preprocessor/2-main.c
@@ -0,0 +1,14 @@
+#include <stdlib.h>
+#include "holberton.h"
+#include <stdio.h>
+/**
+ * main - entry
+ *
+ * Return: 0
+ *
+ */
+int main(void)
+{
+	printf("%s\n", __BASE_FILE__);
+	return (0);
+}
