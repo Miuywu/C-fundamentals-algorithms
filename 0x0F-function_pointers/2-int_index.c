f3f5544386a4d1f2be39b51be9540a129f23a91f
Minh-Huy Vu <49765719+Miuywu@users.noreply.github.com>
2019-07-24 12:55:01 -0700

    add check if ptrs are null

--- 0x0F-function_pointers/2-int_index.c
+++ 0x0F-function_pointers/2-int_index.c
@@ -16,8 +16,9 @@ int int_index(int *array, int size, int (*cmp)(int))
 
 	if (size <= 0)
 		return (-1);
-	for (a = 0; a < size; a++)
-		if (cmp(array[a]) != 0)
-			return (a);
+	if (array != NULL && cmp != NULL)
+		for (a = 0; a < size; a++)
+			if (cmp(array[a]) != 0)
+				return (a);
 	return (-1);
 }
