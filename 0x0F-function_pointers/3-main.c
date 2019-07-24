68dda0cc4bab8d13ad758b02473916d29e0da261
Miuywu <859@holbertonschool.com>
2019-07-24 22:38:58 +0000

    moved 99 exit to after 100

--- 0x0F-function_pointers/3-main.c
+++ 0x0F-function_pointers/3-main.c
@@ -20,16 +20,16 @@ int main(int argc, char *argv[])
 		printf("Error\n");
 		exit(98);
 	}
-	if (argv[2][1] != '\0')
-	{
-		printf("Error\n");
-		exit(99);
-	}
 	if ((argv[2][0] == '/' || argv[2][0] == '%') && b == 0)
 	{
 		printf("Error\n");
 		exit(100);
 	}
+	if (argv[2][1] != '\0')
+        {
+                printf("Error\n");
+                exit(99);
+        }
 	printf("%d\n", (*get_op_func)(argv[2])(a, b));
 	return (0);
 }
