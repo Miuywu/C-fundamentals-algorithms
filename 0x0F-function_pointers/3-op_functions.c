b79e4caae6dd86db3f4befda816b7302575540ae
Miuywu <859@holbertonschool.com>
2019-07-24 22:23:25 +0000

    ops

---
+++ 0x0F-function_pointers/3-op_functions.c
@@ -0,0 +1,58 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "3-calc.h"
+
+/**
+ * op_add - addition
+ * @a: int
+ * @b: int
+ * Return: n
+ */
+int op_add(int a, int b)
+{
+	return (a + b);
+}
+/**
+ * op_sub - subtraction
+ * @a: int
+ * @b: int
+ * Return: n
+ */
+
+int op_sub(int a, int b)
+{
+	return (a - b);
+}
+/**
+ * op_mul - multiply
+ * @a: int
+ * @b: int
+ * Return: n
+ */
+
+int op_mul(int a, int b)
+{
+	return (a * b);
+}
+/**
+ * op_div - division
+ * @a: int
+ * @b: int
+ * Return: n
+ */
+
+int op_div(int a, int b)
+{
+	return (a / b);
+}
+/**
+ * op_mod - remainder
+ * @a: int
+ * @b: int
+ * Return: n
+ */
+
+int op_mod(int a, int b)
+{
+	return (a % b);
+}
