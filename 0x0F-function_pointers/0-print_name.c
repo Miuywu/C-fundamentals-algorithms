ff506a668c136045bd9dc7700e11cb6d71aa1a78
Miuywu <859@holbertonschool.com>
2019-07-24 19:39:02 +0000

    calls to fn in main using ptr

---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "function_pointers.h"
+
+/**
+ * print_name - calls to fn pointer of fn that prints name
+ * @name: n
+ * @f: fn pointer
+ * Return: void
+ */
+
+void print_name(char *name, void (*f)(char *))
+{
+	f(name);
+}
