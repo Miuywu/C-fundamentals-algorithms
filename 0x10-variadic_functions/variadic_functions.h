f8523e424aa9a2f338534e4292ecdd9f2a9eb35a
Miuywu <859@holbertonschool.com>
2019-07-27 20:59:25 +0000

    header

---
+++ 0x10-variadic_functions/variadic_functions.h
@@ -0,0 +1,9 @@
+#ifndef _v_fn_
+#define _v_fn_
+
+int sum_them_all(const unsigned int n, ...);
+void print_numbers(const char *separator, const unsigned int n, ...);
+void print_strings(const char *separator, const unsigned int n, ...);
+void print_all(const char * const format, ...);
+
+#endif
