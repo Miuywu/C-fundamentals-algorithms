20adc54126f16a4b301205b7ea82e640b1a6bce4
Minh-Huy Vu <49765719+Miuywu@users.noreply.github.com>
2019-07-28 23:53:02 -0700

    added nil condition

--- 0x10-variadic_functions/2-print_strings.c
+++ 0x10-variadic_functions/2-print_strings.c
@@ -13,11 +13,16 @@ void print_strings(const char *separator, const unsigned int n, ...)
 {
 	va_list arguments;
 	unsigned int a;
+	char *s;
 
 	va_start(arguments, n);
 	for (a = 0; a < n; a++)
 	{
-		printf("%s", va_arg(arguments, char *));
+		s = va_arg(arguments, char *);
+		if (s == NULL)
+			printf("(nil)");
+		else
+			printf("%s", s);
 		if (separator != NULL && a < (n - 1))
 			printf("%s", separator);
 	}
