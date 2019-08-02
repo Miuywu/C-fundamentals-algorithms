4dbb44bbb39b1f3ee2203d81a841213aecc403cb
Miuywu <859@holbertonschool.com>
2019-08-02 02:35:19 +0000

    returns len of list

---
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -0,0 +1,22 @@
+#include "lists.h"
+/**
+ * print_list - prints all the elements of a list_t list
+ * @h: h
+ * Return: size_t - number of nodes
+ */
+size_t list_len(const list_t *h)
+{
+	size_t size;
+	const list_t *curr = h;
+
+	size = 0;
+	if (h)
+	{
+		while (curr != NULL)
+		{
+			curr = curr->next;
+			size++;
+		}
+	}
+	return (size);
+}
