8cef5b126d1ac3a6f2635d7e931c31492a4dcf41
Miuywu <859@holbertonschool.com>
2019-08-02 04:58:33 +0000

    recursively accesses list_t elements to free them in reverse order

---
+++ 0x12-singly_linked_lists/4-free_list.c
@@ -0,0 +1,17 @@
+#include "lists.h"
+/**
+ * free_list - frees a list_t list
+ * @head: head elem
+ *
+ * Return: address of the new element, or NULL
+ */
+void free_list(list_t *head)
+{
+	if (head != NULL)
+	{
+		free_list(head->next);
+		free(head->str);
+		free(head);
+	}
+	return;
+}
