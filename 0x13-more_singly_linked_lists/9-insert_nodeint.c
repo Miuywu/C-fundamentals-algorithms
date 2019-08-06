e12453677dfc47331e27fad4ce48692545336a57
Miuywu <859@holbertonschool.com>
2019-08-06 20:17:40 +0000

    added null return

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -35,7 +35,10 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 		{
 			curr = curr->next;
 			if (!curr)
+			{
 				free(new);
+				return (NULL);
+			}
 		}
 		new->next = curr->next;
 		curr->next = new;
