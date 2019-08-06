1d1105a8b2eef846fe9d8ea1698e5bdf47a24258
Miuywu <859@holbertonschool.com>
2019-08-06 17:47:09 +0000

    fixed comments

--- 0x13-more_singly_linked_lists/2-add_nodeint.c
+++ 0x13-more_singly_linked_lists/2-add_nodeint.c
@@ -2,7 +2,7 @@
 /**
  * add_nodeint - adds a new node at the beginning of a listint_t list
  * @head: head elem
- * @str: head str
+ * @a: head str
  * Return: address of the new element, or NULL
  */
 listint_t *add_nodeint(listint_t **head, const int n)
