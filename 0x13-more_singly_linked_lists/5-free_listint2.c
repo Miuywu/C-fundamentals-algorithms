bd8509024f6dafd5ab09482a1caa305fc3bd5fc8
Miuywu <859@holbertonschool.com>
2019-08-06 17:52:38 +0000

    added check for head

--- 0x13-more_singly_linked_lists/5-free_listint2.c
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -15,14 +15,20 @@ void free_list(listint_t *head)
 	}
 }
 /**
- * free_listint - frees a listint_t list
+ * free_listint2 - frees a listint_t list
  * @head: head elem
  *
  * Return: address of the new element, or NULL
  */
 void free_listint2(listint_t **head)
 {
-	listint_t *cpy = *head;
+	listint_t *cpy;
+
+
+	if (head == NULL)
+		return;
+
+	cpy = *head;
 	if (cpy != NULL)
 	{
 		free_list(cpy);
