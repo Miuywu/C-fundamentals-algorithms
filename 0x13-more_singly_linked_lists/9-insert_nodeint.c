e1e6e089d0f87571a864d61b73a0df6f99d79d52
Miuywu <859@holbertonschool.com>
2019-08-07 03:28:11 +0000

    added index 0 case

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -17,6 +17,21 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 	if (!new)
 		return (NULL);
 	new->n = n;
+	curr = *head;
+	if (idx == 0)
+	{
+		new->next = curr;
+		*head = new;
+		return (*head);
+	}
+	else
+	{
+		if (!*head)
+		{
+			free (new);
+			return (NULL);
+		}
+	}
 	for (curr = *head; idx > 1; idx--)
 	{
 		curr = curr->next;
