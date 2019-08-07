5697029039e73101eb1798ec48489d4e4de2393d
Miuywu <859@holbertonschool.com>
2019-08-07 04:02:44 +0000

    changed loop cond again

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -11,6 +11,8 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 {
 	listint_t *new, *curr;
 
+	if (!head)
+		return (NULL);
 	new = malloc(sizeof(listint_t));
 	if (!new)
 		return (NULL);
