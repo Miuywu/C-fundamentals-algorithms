35b9c12457388be6224d82d27aa7d313a5722fa5
Miuywu <859@holbertonschool.com>
2019-08-08 20:42:11 +0000

    converts binary to unsigned

---
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -0,0 +1,41 @@
+#include "holberton.h"
+/**
+ * _pow_recursion - raises x to power of y
+ * Return: int factorial of a given number
+ * @x: intput
+ * @y: power
+ */
+unsigned int pow_r(int x, int y)
+{
+	if (y != 0)
+		return (x * pow_r(x, y - 1));
+	else
+		return (1);
+}
+/**
+ * binary_to_uint - converts a binary number to an unsigned int
+ * @b: string of 0's/1's
+ * Return: binary string converted to decimal
+ */
+unsigned int binary_to_uint(const char *b)
+{
+	unsigned int converted = 0;
+	int a = 0, len = 0;
+
+	while (*b == '0')
+		b++;
+	while (b[len])
+	{
+		if (b[len] > '9' || b[len] < '0')
+			return (0);
+		len++;
+	}
+	len--;
+	printf("run %d times.\n", len);
+	for (a = 0; len >= 0; a++, len--)
+	{
+		if (b[len] == '1')
+			converted += pow_r(2, a);
+	}
+	return (converted);
+}
