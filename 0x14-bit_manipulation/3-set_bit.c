c8cf8b1ebd39089c736819a1b2b3ea09bd715f32
Miuywu <859@holbertonschool.com>
2019-08-09 06:37:40 +0000

    shift to index and || to set to 1

---
+++ 0x14-bit_manipulation/3-set_bit.c
@@ -0,0 +1,19 @@
+#include "holberton.h"
+/**
+ * set_bit - prints binary ver of decimal num
+ * @n: n
+ * @index: pos
+ * Return: void
+ */
+int set_bit(unsigned long int *n, unsigned int index)
+{
+	unsigned int len = sizeof(n) * 8;
+	unsigned long int sh;
+
+	if (len <= index)
+		return (-1);
+	len = 0;
+	sh = 1 << index;
+	*n |= sh;
+	return (1);
+}
