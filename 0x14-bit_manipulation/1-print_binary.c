e095d579e2064acfc951d5934e34771f0bb95130
Miuywu <859@holbertonschool.com>
2019-08-09 03:36:02 +0000

    scans bits of uns int & convert to dec

---
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -0,0 +1,36 @@
+#include "holberton.h"
+/**
+ * print_binary - prints binary ver of decimal num
+ * @n: n
+ * Return: void
+ */
+void print_binary(unsigned long int n)
+{
+	int len = 0;
+	unsigned long int mark, r_sh;
+
+	if (!n)
+	{
+		printf("\nran %d times.\n", len);
+		_putchar('0');
+		return;
+	}
+	len = sizeof(n)* 8 - 1;
+	mark = 0;
+	printf("\nran %d times.\n", len);
+	while (len >= 0)
+	{
+		r_sh = n >> len;
+		if (r_sh & 1)
+		{
+			mark = 1;
+			_putchar('1');
+		}
+		else
+		{
+			if (mark)
+				_putchar('0');
+		}
+		len--;
+	}
+}
