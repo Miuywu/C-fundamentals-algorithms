199a030bde8a7caa83dfd33e1fe707a072580967
Miuywu <859@holbertonschool.com>
2019-08-09 06:25:13 +0000

    shifts to index to find bit

---
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -0,0 +1,20 @@
+#include "holberton.h"
+/**
+ * get_bit - prints binary ver of decimal num
+ * @n: n
+ * Return: void
+ */
+int get_bit(unsigned long int n, unsigned int index)
+{
+	unsigned int len = sizeof(n) * 8;
+	unsigned long int sh;
+
+	if (len <= index)
+		return (-1);
+	len = 0;
+	sh = 1 << index;
+	if (sh & n)
+		return (1);
+	else
+		return (0);
+}
