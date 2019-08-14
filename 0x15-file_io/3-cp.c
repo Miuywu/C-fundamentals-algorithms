512059a654c21ef616288cb93db18110b36c2c69
Miuywu <859@holbertonschool.com>
2019-08-14 06:10:51 +0000

    reorganize and added loop account for > 1024

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -1,18 +1,38 @@
 #include "holberton.h"
 /**
+ * writeErr - v
+ * @fd: f
+ */
+void writeErr(char *fd)
+{
+	dprintf(STDERR_FILENO, "Error: Can't write to %s\n", fd);
+	exit(99);
+}
+/**
+ * readErr - v
+ * @fd: f
+ */
+void readErr(char *fd)
+{
+	dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", fd);
+	exit(98);
+}
+/**
+ * exit97 - v
+ */
+void exit97(void)
+{
+	dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n");
+	exit(97);
+}
+/**
  * closeh - v
  * @fd: f
- * @clz: c
  */
-void closeh(int fd, int clz)
+void closeh(int fd)
 {
-	char *err3 = "Error: Can't close fd ";
-
-	if (clz == -1)
-	{
-		dprintf(STDERR_FILENO, "%s%d\n", err3, fd);
-		exit(100);
-	}
+	dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fd);
+	exit(100);
 }
 /**
  * main - creates a file
@@ -22,45 +42,33 @@ void closeh(int fd, int clz)
  */
 int main(int argc, char *argv[])
 {
-	int fd, check, value, fd2, clz;
+	int fd1, value, fd2, clz;
 	char arr[1024];
-	char *err1 = "Error: Can't read from file ";
-	char *err2 = "Error: Can't write to ";
 
-	if (argc != 3) /*WORKS*/
-	{
-		write(STDERR_FILENO, "Usage: cp file_from file_to\n", 28);
-		exit(97);
-	} /*OPEN FILE 1*/
-	fd = open(argv[1], O_RDWR, 0664);
-	if (fd == -1)
-	{ /*works*/
-		dprintf(STDOUT_FILENO, "%s%s\n", err1, argv[1]);
-		exit(98);
-	} /*CREATE FILE 2*/
-	fd2 = open(argv[2], O_CREAT | O_RDWR | O_TRUNC, 0664);
-	if (fd2 == -1)
-	{ /*works*/
-		dprintf(STDOUT_FILENO, "%s%s\n", err2, argv[2]);
-		exit(99);
-	} /*READ FILE 1*/
-	value = read(fd, arr, 1024);
-	if (value == -1)
-	{ /*works*/
-		clz = close(fd);
-		closeh(fd, clz);
-		dprintf(STDOUT_FILENO, "%s%s\n", err1, argv[1]);
-		exit(98);
-	} /*WRITE*/
-	check = write(fd2, arr, value);
-	if (check == -1)
-	{ /*works*/
-		clz = close(fd2);
-		closeh(fd2, clz);
-		dprintf(STDOUT_FILENO, "%s%s\n", err2, argv[2]);
-		exit(99);
+	if (argc != 3)/*WORKS*/
+		exit97();
+	/*OPEN FILE 1*/
+	fd1 = open(argv[1], O_RDONLY, 0664);
+	if (fd1 == -1)/*works*/
+		readErr(argv[1]);
+	/*CREATE FILE 2*/
+	fd2 = open(argv[2], O_CREAT | O_WRONLY | O_TRUNC, 0664);
+	if (fd2 == -1)/*works*/
+		writeErr(argv[2]);
+	/*READ FILE 1*/
+	while ((value = read(fd1, arr, 1024)) > 0)
+	{ /*WRITE*/
+		if (write(fd2, arr, value) != value)/*works*/
+			writeErr(argv[2]);
 	}
-	clz = close(fd);
-	closeh(fd, clz);
+	if (value == -1)
+		readErr(argv[1]);
+	/*CLOSE*/
+	clz = close(fd1);
+	if (clz == -1)/*works*/
+		closeh(fd1);
+	clz = close(fd2);
+	if (clz == -1)/*works*/
+		closeh(fd2);
 	return (0);
 }
