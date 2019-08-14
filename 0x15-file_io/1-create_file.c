f8cbf7c7c184acbfa4812b72e085afeb94fb01c5
Miuywu <859@holbertonschool.com>
2019-08-14 04:14:37 +0000

    fix logic check order

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -14,13 +14,13 @@ int create_file(const char *filename, char *text_content)
 	if (!filename)
 		return (-1);
 
-	fd = open(filename, O_CREAT | O_WRONLY |O_TRUNC, 0600);
+	fd = open(filename, O_CREAT | O_WRONLY | O_TRUNC, 0600);
 	if (fd == -1)
 		return (-1);
+	if (!text_content)
+		return (1);
 	for (value = 0; *filespace != '\0'; value++, filespace++)
 		;
-	if (!text_content)
-		text_content = "";
 	check = write(fd, text_content, value);
 	if (check == -1)
 	{
