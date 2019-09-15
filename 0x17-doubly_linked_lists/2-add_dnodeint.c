bab541b8f2a545889a1af9fd540a48aca84ef9b0
miuywu <859@holbertonschool.com>
2019-09-15 02:10:32 +0000

    added check for existence of list

--- 0x17-doubly_linked_lists/2-add_dnodeint.c
+++ 0x17-doubly_linked_lists/2-add_dnodeint.c
@@ -9,13 +9,20 @@ dlistint_t *add_dnodeint(dlistint_t **head, const int n)
 {
 	dlistint_t *new;
 
-	new = malloc(sizeof(dlistint_t));
-	if (new)
+	if (head)
 	{
-		new->n = n;
-		new->next = *head;
-		*head = new;
-		return (new);
+		new = malloc(sizeof(dlistint_t));
+		if (new)
+		{
+			if (*head)
+			{
+				new->n = n;
+				new->next = *head;
+				(*head)->prev = new;
+			}
+			*head = new;
+			return (new);
+		}
 	}
 	return (NULL);
 }
