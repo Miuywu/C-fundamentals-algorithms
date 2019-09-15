bfd3ed64d5f77ede34dddfdaf943232c32dcdb8b
miuywu <859@holbertonschool.com>
2019-09-15 00:15:37 +0000

    finds len of linked list

---
+++ 0x17-doubly_linked_lists/1-dlistint_len.c
@@ -0,0 +1,22 @@
+#include "lists.h"
+/**
+ * dlistint_len - returns len of dlist_int
+ * @h: head of linked list
+ * Return: size_t - number of nodes
+ */
+size_t print_dlistint(const dlistint_t *h)
+{
+	size_t size;
+	const dlistint_t *curr = h;
+
+	size = 0;
+	if (h)
+	{
+		while (curr != NULL)
+		{
+			curr = curr->next;
+			size++;
+		}
+	}
+	return (size);
+}
