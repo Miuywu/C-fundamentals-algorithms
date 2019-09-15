ffe7093c6494f2314aacecc77da564632bdeba15
miuywu <859@holbertonschool.com>
2019-09-15 20:12:37 +0000

    fixed var name

--- 0x17-doubly_linked_lists/3-add_dnodeint_end.c
+++ 0x17-doubly_linked_lists/3-add_dnodeint_end.c
@@ -24,7 +24,7 @@ dlistint_t *add_dnodeint_end(dlistint_t **head, const int n)
 		while (cpy->next)
 			cpy = cpy->next;
 		cpy->next = new;
-		new->prev = copy;
+		new->prev = cpy;
 		return (*head);
 	}
 	return (NULL);
