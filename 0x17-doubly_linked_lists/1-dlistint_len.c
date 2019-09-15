6f50499c3fe8ec6a20ebf5328ae15c3f24f0d1f1
miuywu <859@holbertonschool.com>
2019-09-15 00:17:30 +0000

    finds len of linked list

--- 0x17-doubly_linked_lists/1-dlistint_len.c
+++ 0x17-doubly_linked_lists/1-dlistint_len.c
@@ -4,7 +4,7 @@
  * @h: head of linked list
  * Return: size_t - number of nodes
  */
-size_t print_dlistint(const dlistint_t *h)
+size_t dlistint_len(const dlistint_t *h)
 {
 	size_t size;
 	const dlistint_t *curr = h;
