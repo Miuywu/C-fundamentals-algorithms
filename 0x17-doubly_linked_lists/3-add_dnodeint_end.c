3f3ccd02f6320e599df2fc6fdf3d05ef70807800
miuywu <859@holbertonschool.com>
2019-09-15 20:11:23 +0000

    initialized new->prev

--- 0x17-doubly_linked_lists/3-add_dnodeint_end.c
+++ 0x17-doubly_linked_lists/3-add_dnodeint_end.c
@@ -24,6 +24,7 @@ dlistint_t *add_dnodeint_end(dlistint_t **head, const int n)
 		while (cpy->next)
 			cpy = cpy->next;
 		cpy->next = new;
+		new->prev = copy;
 		return (*head);
 	}
 	return (NULL);
