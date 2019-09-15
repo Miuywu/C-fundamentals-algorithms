44dadd2c81c07886b1d6061aa6acf106a1c960c2
miuywu <859@holbertonschool.com>
2019-09-15 20:32:51 +0000

    finds the sum of all the node values in dlistint_t

---
+++ 0x17-doubly_linked_lists/6-sum_dlistint.c
@@ -0,0 +1,22 @@
+#include "lists.h"
+/**
+ * sum_dlistint - returns the sum of all the data (n) of a listint_t linked list
+ * @head: h
+ * Return: size_t - number of nodes
+ */
+int sum_listint(dlistint_t *head)
+{
+	int sum;
+	dlistint_t *curr = head;
+
+	sum = 0;
+	if (head)
+	{
+		while (curr != NULL)
+		{
+			sum += curr->n;
+			curr = curr->next;
+		}
+	}
+	return (sum);
+}
