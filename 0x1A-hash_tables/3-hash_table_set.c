a543e10f708026820a25fb478e7880d24e7011fa
Miuywu <859@holbertonschool.com>
2019-10-11 20:26:06 +0000

    adds an key/value to hash table

---
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -0,0 +1,41 @@
+#include "hash_tables.h"
+/**
+ * hash_table_set - adds an element to hash table
+ * @ht: hash table
+ * @key: key of pair
+ * @value: value of pair
+ */
+
+int hash_table_set(hash_table_t *ht, const char *key, const char *value)
+{
+	unsigned char * k_key;
+	unsigned long int i, size;
+	hash_node_t *head;
+
+	if (ht || key || value)
+		return (0);
+	k_key = (unsigned char *)key;
+	size = ht->size;
+	i = key_index(k_key, size);
+	for (head = ht->array[i]; head; head = head->next)
+		if (!strcmp(head->key, key))
+		{
+			free(head->value);
+			head->value = strcpy(head->value, value);
+			if (strcmp(head->value, value) != 0)
+				return (0);
+			return (1);
+		}
+	head = malloc(sizeof(hash_node_t));
+	if (!head)
+		return (0);
+	head->key = strcpy(head->key, key);
+	if (strcmp(head->key, key) != 0)
+		return (0);
+	head->value = strcpy(head->value, value);
+	if (strcmp(head->value, value) != 0)
+		return (0);
+	head->next = ht->array[i];
+	ht->array[i] = head;
+	return (1);
+}
