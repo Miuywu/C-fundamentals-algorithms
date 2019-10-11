bdc9961be16c09433b0892aba0c139df27fd2cb7
Miuywu <859@holbertonschool.com>
2019-10-11 21:40:49 +0000

    deladdnode strcpy->strdup

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -1,28 +1,4 @@
 #include "hash_tables.h"
-hash_node_t *add_node_hash(hash_node_t **head, const char *str, const char *k);
-
-/**
- * add_node_hash - adds a new node at the beginning of a list_t list
- * @head: head elem
- * @str: head str
- * @k: key
- * Return: address of the new element, or NULL
- */
-hash_node_t *add_node_hash(hash_node_t **head, const char *str, const char *k)
-{
-	hash_node_t *new;
-
-	new = malloc(sizeof(hash_node_t));
-	if (new)
-	{
-		new->value = strdup(str);
-		new->next = *head;
-		new->key = strdup(k);
-		*head = new;
-		return (new);
-	}
-	return (NULL);
-}
 /**
  * hash_table_set - adds an element to hash table
  * @ht: hash table
@@ -45,11 +21,11 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 	for (head = ht->array[i]; head; head = head->next)
 		if (!strcmp(head->key, key))
 		{
-			head = ht->array[i];
-			head = add_node_hash(&head, value, key);
-			if (!head)
-				return (0);
-			head->next = ht->array[i];
+			free(ht->array[i]->value);
+			ht->array[i]->value = strdup(head->value);
+			free(head->value);
+			free(head->key);
+			free(head);
 			return (1);
 		}
 	head = malloc(sizeof(hash_node_t));
