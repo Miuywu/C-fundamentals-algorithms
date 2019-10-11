0df31ab04398912dff5b2db414dca289e7cf0f2b
Miuywu <859@holbertonschool.com>
2019-10-11 22:04:21 +0000

    try again

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -11,33 +11,24 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 {
 	unsigned char *k_key;
 	unsigned long int i, size;
-	hash_node_t *head;
+	hash_node_t *new;
 
 	if (ht || key || value)
 		return (0);
 	k_key = (unsigned char *)key;
 	size = ht->size;
+
 	i = key_index(k_key, size);
-	for (head = ht->array[i]; head; head = head->next)
-		if (!strcmp(head->key, key))
-		{
-			free(ht->array[i]->value);
-			ht->array[i]->value = strdup(head->value);
-			free(head->value);
-			free(head->key);
-			free(head);
-			return (1);
-		}
-	head = malloc(sizeof(hash_node_t));
-	if (!head)
+	if (i > sizeof(ht->array) / sizeof(hash_node_t *))
 		return (0);
-	head->key = strcpy(head->key, key);
-	if (strcmp(head->key, key) != 0)
+
+	new = malloc(sizeof(hash_node_t));
+	if (!new)
 		return (0);
-	head->value = strcpy(head->value, value);
-	if (strcmp(head->value, value) != 0)
-		return (0);
-	head->next = ht->array[i];
-	ht->array[i] = head;
+	new->key = strcpy(new->key, key);
+	new->value = strcpy(new->value, value);
+
+	new->next = ht->array[i];
+	ht->array[i] = new;
 	return (1);
 }
