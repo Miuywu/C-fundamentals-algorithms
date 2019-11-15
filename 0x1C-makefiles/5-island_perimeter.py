874a640dbdc826622f348c860b2a45f12cd37f0a
Miuywu <859@holbertonschool.com>
2019-11-15 15:49:09 -0800

    find perimeter of island of 1's in sea of 0's

---
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+
+def island_perimeter(grid):
+    p = 0
+    for a in range(len(grid)):
+        for b in range(len(grid[a]) - 1):
+            if grid[a][b] == 1:
+                p += 1
+                break
+        for b in range(len(grid) - 1, 0, -1):
+            if grid[a][b] == 1:
+                p += 1
+                break
+    for a in range(len(grid)):
+        for b in range(len(grid[a]) - 1):
+            if grid[b - 1][a] == 1:
+                p += 1
+                break
+        for b in range(len(grid) - 1, 0, -1):
+            if grid[b][a] == 1:
+                p += 1
+                break
+    return p
