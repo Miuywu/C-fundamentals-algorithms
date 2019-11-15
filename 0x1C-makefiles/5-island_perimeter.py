a8d71350c663bdab90755c71471deec8549429af
Miuywu <859@holbertonschool.com>
2019-11-15 15:53:30 -0800

    changed index on loops

--- 0x1C-makefiles/5-island_perimeter.py
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -2,7 +2,7 @@
 
 def island_perimeter(grid):
     p = 0
-    for a in range(len(grid)):
+    for a in range(len(grid) - 1):
         for b in range(len(grid[a]) - 1):
             if grid[a][b] == 1:
                 p += 1
@@ -11,7 +11,7 @@ def island_perimeter(grid):
             if grid[a][b] == 1:
                 p += 1
                 break
-    for a in range(len(grid)):
+    for a in range(len(grid) - 1):
         for b in range(len(grid[a]) - 1):
             if grid[b - 1][a] == 1:
                 p += 1
