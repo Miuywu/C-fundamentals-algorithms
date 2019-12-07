c9c0172e5e0523e45c5a3e8df155ee401bbf145c
Miuywu <859@holbertonschool.com>
2019-12-07 02:11:48 +0000

    updated header

--- 0x1D-binary_trees/binary_trees.h
+++ 0x1D-binary_trees/binary_trees.h
@@ -68,4 +68,5 @@ binary_tree_t *binary_tree_rotate_right(binary_tree_t *tree);
 int binary_tree_is_bst(const binary_tree_t *tree);
 bst_t *bst_insert(bst_t **tree, int value);
 
+int binary_tree_is_avl(const binary_tree_t *tree);
 #endif
