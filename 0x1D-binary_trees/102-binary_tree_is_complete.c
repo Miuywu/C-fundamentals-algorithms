d349130029a07705efe638c1a0c159891a53dfce
Miuywu <859@holbertonschool.com>
2019-12-07 04:26:31 +0000

    update to match shared dir

--- 0x1D-binary_trees/102-binary_tree_is_complete.c
+++ 0x1D-binary_trees/102-binary_tree_is_complete.c
@@ -1,8 +1,32 @@
 #include "binary_trees.h"
-int i_this_binary_tree_height(const binary_tree_t *tree, int rootval);
-int i_binary_tree_preorder(const binary_tree_t *tree, int flag);
-int i_recurse(const binary_tree_t *tree);
-int i_binary_tree_balance(const binary_tree_t *tree);
+#include <stdbool.h>
+/**
+ * count_nodes - counts number of nodes in binary tree
+ * @root: root of tree
+ * Return: number of nodes
+ */
+int count_nodes(const binary_tree_t *root)
+{
+	if (root == NULL)
+		return (0);
+	return (1 + count_nodes(root->left) + count_nodes(root->right));
+}
+/**
+ * is_complete - checks if tree has size - 1 nodes
+ * @root: root of tree
+ * @index: index assigned to node
+ * @number_nodes: number of nodes
+ * Return: true if tree is complete, false otherwise
+ */
+bool is_complete(const binary_tree_t *root, int index, int number_nodes)
+{
+	if (root == NULL)
+		return (true);
+	if (index >= number_nodes)
+		return (false);
+	return (is_complete(root->left, 2 * index + 1, number_nodes) &&
+		is_complete(root->right, 2 * index + 2, number_nodes));
+}
 /**
  * binary_tree_is_complete - checks if a binary tree is complete
  * @tree: root of tree
@@ -10,95 +34,15 @@ int i_binary_tree_balance(const binary_tree_t *tree);
  */
 int binary_tree_is_complete(const binary_tree_t *tree)
 {
-	int finalbalance;
+	int node_count, index;
 
 	if (!tree)
 		return (0);
-	finalbalance = i_binary_tree_preorder(tree, 0);
-/*	printf("final = %d\n", finalbalance);*/
 
-	if (finalbalance < 2 && finalbalance > -1)
+	node_count = count_nodes(tree);
+	index = 0;
+	if (is_complete(tree, index, node_count))
 		return (1);
-	return (0);
-}
-/**
- * i_binary_tree_preorder - iterates a tree in pre-order traversal.
- * @tree: The pointer to the root node of the tree to traverse.
- * @flag: balance factor
- * Return: flag
- */
-int i_binary_tree_preorder(const binary_tree_t *tree, int flag)
-{
-	int balance, flag1;
-
-	if (!tree)
-		return (flag);
-/*	printf("(%d) | FLAG %d\n", tree->n, flag);*/
-	if (flag == 2)
-		return (2);
-	flag1 = i_binary_tree_preorder(tree->left, flag);
-	if (flag1 == -1)
-		return (-1);
-	flag1 += i_binary_tree_preorder(tree->right, flag);
-	if (flag1 == -1)
-		return (-1);
-	balance = i_binary_tree_balance(tree);
-/*	printf("(%d) balance %d\n", tree->n, balance);*/
-	if (balance == -1)
-		return (-1);
-
-	flag += balance;
-/**/
-	if (flag > 1)
-		return (2);
-	return (flag - flag1); /*1 or 0*/
-}
-/**
- * i_binary_tree_balance - Measures the balance factor of a binary tree.
- * @tree: The pointer to the root of the tree.
- * Return: The balance factor of the tree.
- */
-int i_binary_tree_balance(const binary_tree_t *tree)
-{
-	int l_height = 0, r_height = 0;
-
-	if (!tree)
+	else
 		return (0);
-	if (tree->left)
-		l_height = 1 + i_this_binary_tree_height(tree->left, tree->left->n);
-	if (tree->right)
-		r_height = 1 + i_this_binary_tree_height(tree->right, tree->right->n);
-	return (l_height - r_height);
-}
-/**
- * i_recurse - Measures the height of the entire binary tree.
- * @tree: The pointer to the root node of the tree to measure the height.
- * Return: The height of a binary tree.
- */
-int i_recurse(const binary_tree_t *tree)
-{
-	int left_height, right_height;
-
-	if (!tree)
-		return (0);
-	left_height = i_recurse(tree->left) + 1;
-	right_height = i_recurse(tree->right) + 1;
-	if (left_height > right_height)
-		return (left_height);
-	return (right_height);
-}
-
-/**
- * i_this_binary_tree_height - Measures the height of a binary tree.
- * @tree: The pointer to the root node of the tree to measure the height.
- * @rootval: The value of the root head.
- * Return: The height of a binary tree.
- */
-int i_this_binary_tree_height(const binary_tree_t *tree, int rootval)
-{
-	int depth;
-
-	for (depth = 0; tree && tree->n != rootval; tree = tree->parent, depth++)
-		;
-	return (i_recurse(tree) - 1 - depth);
 }
